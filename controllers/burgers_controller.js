8ca124f199ff0895d42e2cc066a39dabc29ab931
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-01-03 16:29:46 -0800

    created file structure

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,13 @@
+let express = require("express");
+
+let router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+let burger = require("../models/burger.js");
+
+
+// Create the router for the app
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('French Onion Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
